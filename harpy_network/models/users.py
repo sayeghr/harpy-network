61dd259a58f230ff920e0e777f19aedb6e80e7a8
Riziq Sayegh <sayeghr@neurohack.ca>
2015-05-13 19:34:05 -0400

    Added a basic user model. Added bcypt password hashing. Added user tests.

---
+++ harpy_network/models/users.py
@@ -0,0 +1,21 @@
+from passlib.hash import bcrypt
+
+from harpy_network import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(254), unique=True)
+    password = db.Column(db.String(60))
+    admin = db.Column(db.Boolean, default=False)
+
+    def __init__(self, email, password, admin=False):
+        self.email = email
+        self.set_password(password)
+        self.admin = admin
+
+    def set_password(self, password):
+        self.password = bcrypt.encrypt(password)
+        return self.password
+
+    def verify_password(self, password):
+        return bcrypt.verify(password, self.password)
