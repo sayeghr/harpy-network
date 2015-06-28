9d7b02cc419cef3be2a1be704b195de0366b4c92
Riziq Sayegh <sayeghr@neurohack.ca>
2015-06-28 09:25:18 -0400

    The prestation clickable links now gets applied everytime the datatable is drawn.

--- harpy_network/static/js/prestation.js
+++ harpy_network/static/js/prestation.js
@@ -1,9 +1,15 @@
 $( document ).ready(function() {
-    $('#prestation-table').dataTable();
+    $('#prestation-table').dataTable( {
+            "drawCallback": function( settings ) {
+                updatePrestationTableLinks();
+            }
+    });
+});
 
+function updatePrestationTableLinks() {
     $('.prestation-row').click(function() {
         // row was clicked
         prestation_id = $(this).data("prestation-id");
         window.location.href = window.location.href + '/' + prestation_id;
     });
-});
+}
